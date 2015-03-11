93d0662d1cb6942def5b825c19ecb034ea44c3c3
Matt Lim <mlim@caltech.edu>
2015-03-10 18:39:33 -0700

    Finish code that parses data.txt

--- Parser.py
+++ Parser.py
@@ -10,6 +10,7 @@ class Parser:
         pass
 
     def parse_movie_data(self, filename):
+        # TODO
 
         print 'Parsing movie data'
         x = []
@@ -22,11 +23,11 @@ class Parser:
             for word in line.split(" "):
                 x.append(word)
 
-        print x
-
 
     def parse_ratings_data(self, filename):
-
+        '''Parses the ratings data, which consists of:
+        a user id, a movie id, a rating
+        '''
 
         print 'Parsing ratings data'
         user_ids = []
@@ -36,15 +37,24 @@ class Parser:
         # Get a handle for the specified file
         data = open(filename)
         for line in data:
-            print line
-            arr = line.split()
+            line = line.rstrip()
+            arr = map(int, line.split('\t'))
             user_ids.append(arr[0])
             movie_ids.append(arr[1])
             ratings.append(arr[2])
-        print user_ids
 
-        return user_ids, movie_ids, ratings
+        num_rows = max(user_ids)
+        num_columns = max(movie_ids)
+        Y = [[0 for i in range(num_columns)] for i in range(num_rows)]
+        for i in range(0, len(user_ids)):
+            user_id = user_ids[i] - 1 # decrement by 1 because matrix is 0 indexed
+            movie_id = movie_ids[i] - 1 # decrement by 1 because matrix is 0 indexed
+            rating = ratings[i]
+            Y[user_id][movie_id] = rating
+
+        return Y
+
 
 if __name__ == '__main__':
     parser = Parser()
-    parser.parse_movie_data('data/data.txt')
+    parser.parse_ratings_data('data/data.txt')
