c32bfd2d987132a779e3f5c1e048a54fa4e63dde
Kshitij Grover <kgrover@caltech.edu>
2015-03-10 18:07:00 -0700

    Parser

---
+++ Parser.py
@@ -0,0 +1,50 @@
+import csv
+import os
+from sklearn.decomposition import PCA
+
+class Parser:
+    '''
+    Parses a tab/space seperated data file
+    '''
+    def __init__(self):
+        pass
+
+    def parse_movie_data(self, filename):
+        
+        print 'Parsing movie data'
+        x = []
+        y = []
+
+        # Get a handle for the specified file
+        movies = open(filename)
+        
+        for line in movies:
+            for word in line.split(" "):
+                x.append(word)
+        
+        print x
+
+
+    def parse_ratings_data(self, filename):
+
+
+        print 'Parsing ratings data'
+        user_ids = []
+        movie_ids = []
+        ratings = []
+
+        # Get a handle for the specified file
+        data = open(filename)
+        
+
+        for line in data:
+            print line
+            arr = line.split()
+            user_ids.append(arr[0])
+            movie_ids.append(arr[1])
+            ratings.append(arr[2])
+        
+        print user_ids
+        
+
+   
